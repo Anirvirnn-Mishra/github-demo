8cbbfc65c2a41e17fb9c254f3004b35ef697d02a
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-18 03:23:17 +0530

    fade out vs fade to

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
-   $(".green-box").fadeOut(2000);
-   $("H2").fadeOut(2000);
-   $("H2").fadeIn(2000); 
-   $(".blue-box").fadeTo(1000, 0.5);
+   $(".red-box").fadeTo(2000 , 0.2);
+   $(".red-box").fadeOut(2000 );// removes the object(display:none) but 
+   $(".green-box").fadeTo(1000 , 0.5);///fade to 0 keeps the element there but invisible
+   $(".blue-box").fadeTo(500 , 0.8);
+   $(".red-box").fadeToggle();
+   $(".red-box").fadeToggle();
+
+   
 });
