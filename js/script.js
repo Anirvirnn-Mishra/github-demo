bde7f0186b8b3009d1646e27576f411757ac304e
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-18 03:17:11 +0530

    fade in fade out simple

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut("slow");
+   $(".red-box").fadeOut(2000);
+   $(".green-box").fadeOut(2000);
+   $("H2").fadeOut(2000);
+   $("H2").fadeIn(2000); 
+   $(".blue-box").fadeTo(1000, 0.5);
 });
