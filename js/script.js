da6dd05a74fcb6c856bd4174137833325d1f1d35
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-19 02:59:31 +0530

    empty remove detach

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-  // replaced with
-  $("ul ul li").replaceWith("mew <br>");
-  $(".red-box, .green-box").replaceWith("<div class='blue-box'>more blue box</div>");
-  $("<div class='green-box'>green returns</div>").replaceAll(".blue-box");
   
+  $("form").children().not("input:text , textarea, br").remove();
+  var detachedItem= $("li").detach();// preserves the attached event handelers and otther data
 
-  
+  $("#content").append(detachedItem);  
 
-  
+  $("p").empty();// does not remove p but empty else everything including children
 
 });
