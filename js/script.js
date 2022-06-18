60127c09fe540951217a2454772732c80ab66489
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-18 16:54:22 +0530

    slding up down and toggle

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 $(function() {
   // jQuery goes here...
 
+  $(".blue-box").slideUp(200);
+  $(".blue-box").slideDown(200);
+  $("P").hide();
+  $("P").slideDown(300);
+  $("p").slideToggle();
 
-   $(".blue-box").hide();
-   $(".blue-box").show();
-   $(".blue-box").hide(1000);
-   $(".blue-box").show(1000);
-   
-   
-   //$(".blue-box").toggle();
 
    
 
