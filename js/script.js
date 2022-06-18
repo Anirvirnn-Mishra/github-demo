9b6f3488c2368f8c9d07cc5afeb65211e2a979ee
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-19 00:08:52 +0530

    css selector

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(function () {
-  $(".lightbox").delay(500).fadeIn(1000);
+  $("p").css("background-color", "#88ffff");  
+  $("input[type=submit]").css("background-color", "#7022ff");  
+  $("h2,input,p").css("color","#11ff11");
+  $("li:last").css("color","#114411"); 
+  $("li:even").css("color","#ff4411"); 
+  
+
 
 });
