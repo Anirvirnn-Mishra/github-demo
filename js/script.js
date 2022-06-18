0f011e889dfb5a4b2eabe7af5bc3f3ca47bc7c7b
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-19 02:43:35 +0530

    replcae with and replace all

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-  // as a child
-   $("ul ul:last").append("<li>last subitem</li>");
-   $("<li>External item</li>").appendTo("ul ul:first");
-   $("<li>External item1</li>").prependTo("ul ul:first");
-   $("ul ul:last").prepend("<li>last subitem1</li>");
-   $("#content").prepend("<h2>before content div</h2>");
+  // replaced with
+  $("ul ul li").replaceWith("mew <br>");
+  $(".red-box, .green-box").replaceWith("<div class='blue-box'>more blue box</div>");
+  $("<div class='green-box'>green returns</div>").replaceAll(".blue-box");
+  
 
-   // as a sibling
-    
-    
-    $(".red-box").before("<div class='yellow-box'>Yellow</div>");
-    $(".red-box").after(function () { 
-      return "<div class='yellow-box'>Yellow</div>";
-     });
-     $(".yellow-box").after($(".blue-box"));// moves the inner element if only one call else it clones
+  
 
   
 
