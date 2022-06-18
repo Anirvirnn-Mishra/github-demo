af14db6427d852c6310f53252e9ea0548db89922
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-19 01:22:23 +0530

    filtering

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-  $("#list").find("li").css("background-color","#00aaff");
-  $("#list").children("li").css("background-color","#aaaaff");
-  $("#list").parents("div").css("color","#ee2222"); 
-  $("#list").siblings("p").css("color","#00ff00");// same level
-  $("#list").prev("p").css("background-color","yellow");
-  $("#list").next("p").css("background-color","yellow");
-  
+  //$("#list").find("li").filter(":even").css("font-weight","bold");// filtered
+  // $("#list").find("li").filter(function (index) 
+  //   {
+  //     if(index===0 )
+  //     {
+  //       return true;
+  //     }
+
+  //   }
+  //   ).css("font-weight","bold");// filtered
+    $("li").eq(3).css("font-weight","bold");// filtered
+    $("li").eq(-2).css("font-weight","bold");// filtered
+    $("li").not(3).css("font-style","italic");// opp of filter
 
 
 });
