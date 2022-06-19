adc446c8a46279b164073532b5d186e9bbaf1776
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-19 16:39:29 +0530

    set interval and image transition

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-  var link=$("#linkk");
-  console.log(link.attr("href"));// retireve href attribute
-  console.log(link.attr("title"));// retireve title  attribute but undefined
-  link.attr("href","/mewo-mewo");// set href attribute
+  var galImage= $(".gallery").find("img").first();
+  var images=["images/laptop-mobile_small.jpg",
+  "images/laptop-on-table_small.jpg",
+  "images/people-office-group-team_small.jpg"];
+  var i=0;
+  setInterval(function () 
+  {
+    i= (i+1)%images.length;
+    galImage.fadeOut(
+      function () 
+      {
+        $(this).attr("src",images[i]); 
+         $(this).fadeIn(1000); });
+         
+         console.log(i);
 
-  // do not use in boolean attributes like disabled, checked etc
-  var checkbox= $("#check");
-  checkbox.prop("checked",true);
-  checkbox.prop("disabled",true);
+  }
+  ,2000);
+  var someP=$("p").find("a").first();
+  setInterval(function () 
+  {
+    i= (i+1)%images.length;
+    someP.fadeOut(
+      function () 
+      {
+        $(this).attr("href",images[i]); 
+         $(this).fadeIn(1000); });
+         
+         console.log(i);
 
-  // val to get values of the elements
-  console.log($("input[type=email]").val());// get
-  $("input[type=email]").val("everythong@akndnv.com");// set
- 
+  }
+  ,2000);
 
-
-  // these all handle one at a time not all elements
 });
