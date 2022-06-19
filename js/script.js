74a19988ffa76dc7c5417f21702bf72cafb3affe
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-19 21:43:15 +0530

    changing css props

--- js/script.js
+++ js/script.js
@@ -1,36 +1,16 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-  var galImage= $(".gallery").find("img").first();
-  var images=["images/laptop-mobile_small.jpg",
-  "images/laptop-on-table_small.jpg",
-  "images/people-office-group-team_small.jpg"];
-  var i=0;
-  setInterval(function () 
-  {
-    i= (i+1)%images.length;
-    galImage.fadeOut(
-      function () 
-      {
-        $(this).attr("src",images[i]); 
-         $(this).fadeIn(1000); });
-         
-         console.log(i);
 
-  }
-  ,2000);
-  var someP=$("p").find("a").first();
-  setInterval(function () 
-  {
-    i= (i+1)%images.length;
-    someP.fadeOut(
-      function () 
-      {
-        $(this).attr("href",images[i]); 
-         $(this).fadeIn(1000); });
-         
-         console.log(i);
+  $("#gallery").css("display","none");
+  var redBox= $(".red-box");
+  redBox.css("width")// returns 80px
+  redBox.width();// returns 80
+redBox.width(1111);// set it to 1111
+$("p").css("font-size", "92px");
+$("p").css("font-size", "+=12px");// relative width , increase
+var props=$("p").css(["font-size", "font-weight"]);// all properties as js object
+redBox.css("user-select","none");
+redBox.css("user-select",function () {return "none";  });
 
-  }
-  ,2000);
 
 });
