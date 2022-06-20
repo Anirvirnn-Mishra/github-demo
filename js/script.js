6bb21dd53df2c4f637b9e9a496384a38a5177459
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-20 14:06:21 +0530

    between  html  tags manipulation, text manipulation

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
+  var pFirst= $("p:first");
+  // get
+  console.log(pFirst.text());
+  console.log(pFirst.html());
 
-  var gallery=$(".gallery");
-  var images=["images/laptop-mobile_small.jpg","images/laptop-on-table_small.jpg","images/people-office-group-team_small.jpg"];
-  gallery.data("availableImages",images);
-  gallery.data("name","something ");
-  
-  console.log(gallery.data());// returns all data associated
-  gallery.removeData("name");// name data does not exist anymore
-
-  console.log($("p:first").data("additional-data"));// prints the property
+  //set
+  pFirst.text("<em>something is changed</em>");// just simply paste the text
+  pFirst.html(pFirst.text()+"<em>something is changed</em>");// parses html and then paste it
 
 
-
-
-
-  // var some=gallery.detach();
-  // $("body").prepend(some);
   
 
 });
