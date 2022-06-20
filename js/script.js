5b6e3cfda0e8b623f4cb0c47dd7303972066f336
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-20 20:37:00 +0530

    on function multiple in one

--- js/script.js
+++ js/script.js
@@ -1,44 +1,25 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
+// on("click",function(){...})
+// $("html").on("click keydown", function (e/**optional event arg */) {
+//   //console.log($(e.offsetX));
+//   console.log(e.key );
+// });// calls everytime click or key is pressed
 
-$("#btn-click").hover(function () {
-    // over
-    $(this).text("hoverd");
-    
-  }
-);
-// alternate 1
-// $(".blue-box").hover(function () {
-//     // over
-//     $(this).fadeTo(200,0.5);
-//     $(this).text("Hovered");
-//   }, function () {
-//     // out
-//     $(this).fadeTo(200,1);
-//     $(this).text("Blue");
-//   }
-// );
-
-// alternate 2
-// $(".blue-box").mouseenter(function () { 
-//   $(this).fadeTo(200,0.5);
-//   $(this).text("Enter");
-//  });
-// $(".blue-box").mouseleave(function () { 
-//   $(this).fadeTo(200,1);
-//   $(this).text("Leave");
-//  });/// this and above might lag when called fast enough that is why below is perfect soln with alternat1 also
-
-
-
-  $(".blue-box").mouseenter(function () {
-    $(this).stop().fadeTo(2000, 0.5);
-    $(this).text("Enter");
-  });
-  $(".blue-box").mouseleave(function () {
-    $(this).stop().fadeTo(2000, 1);
-    $(this).text("Leave");// either this or alternate 1 with stop()
-
+var i =0;
+var images=["images/laptop-mobile_small.jpg","images/people-office-group-team_small.jpg","images/laptop-on-table_small.jpg"];
+// $(".gallery").on("click", function () {
+//   i=(i+1)%images.length;
+//   $(this).find("img").first().attr("src",images[i]).fadeIn(200000); 
+//   //$(this).find("img").first().stop().attr("src",images[i]); // either can be usefd
+// });
+$(".gallery").find("img").first().on("click", function () {
+  i=(i+1)%images.length;
+  $(this).fadeOut(function () {
+  $(this).attr("src",images[i]).fadeIn();}); 
+  //$(this).find("img").first().stop().attr("src",images[i]); // either can be usefd
 });
+
+
 
 });
