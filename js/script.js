1e01399420765bb36f93ac998d87448421582924
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-20 12:04:59 +0530

    add remove css class

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
 
-  $("#gallery").css("display","none");
-  var redBox= $(".red-box");
-  redBox.css("width")// returns 80px
-  redBox.width();// returns 80
-redBox.width(1111);// set it to 1111
-$("p").css("font-size", "92px");
-$("p").css("font-size", "+=12px");// relative width , increase
-var props=$("p").css(["font-size", "font-weight"]);// all properties as js object
-redBox.css("user-select","none");
-redBox.css("user-select",function () {return "none";  });
+  $("a").addClass("fancy-link");// changed class of an element
+
+  $("div").addClass(function (index,currentClass) { 
+    if (currentClass==="dummy") {
+      return "red-box";
+      
+    } 
+   });
+   $(".red-box").removeClass().addClass("yellow-box");
+
 
 
 });
