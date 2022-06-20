e457adbac7b6e33b3a9d25e964a592597e8a170b
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-20 18:34:25 +0530

    add click event listener

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-  var pFirst= $("p:first");
-  // get
-  console.log(pFirst.text());
-  console.log(pFirst.html());
 
-  //set
-  pFirst.text("<em>something is changed</em>");// just simply paste the text
-  pFirst.html(pFirst.text()+"<em>something is changed</em>");// parses html and then paste it
+  $("#btn-click").click(function (e) { // e is event obj
+    $(this).text("CLicked");
+    alert("button click kyu kara?");
+    console.log(e.ctrlKey);// PRESSED WITH CTRL KEY?
+    console.log(e.shiftKey);// PRESSED WITH SHIFT KEY?
 
 
-  
+    
+  });
+  // ADDING CLICK LISTENER
+  $(".red-box").click(function (e) { 
+    e.preventDefault();
+    $(this).fadeTo(1000,0.5);
+  });
+  // triggering event listener
+  $(".red-box").click();
 
 });
