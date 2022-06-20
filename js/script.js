cf314a2b1deef7221b283eff7cf4e25ef615dc16
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-20 20:11:49 +0530

    hover mouse enter mouse leave

--- js/script.js
+++ js/script.js
@@ -1,21 +1,44 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
 
-  $("#btn-click").click(function (e) { // e is event obj
-    $(this).text("CLicked");
-    alert("button click kyu kara?");
-    console.log(e.ctrlKey);// PRESSED WITH CTRL KEY?
-    console.log(e.shiftKey);// PRESSED WITH SHIFT KEY?
-
-
+$("#btn-click").hover(function () {
+    // over
+    $(this).text("hoverd");
     
+  }
+);
+// alternate 1
+// $(".blue-box").hover(function () {
+//     // over
+//     $(this).fadeTo(200,0.5);
+//     $(this).text("Hovered");
+//   }, function () {
+//     // out
+//     $(this).fadeTo(200,1);
+//     $(this).text("Blue");
+//   }
+// );
+
+// alternate 2
+// $(".blue-box").mouseenter(function () { 
+//   $(this).fadeTo(200,0.5);
+//   $(this).text("Enter");
+//  });
+// $(".blue-box").mouseleave(function () { 
+//   $(this).fadeTo(200,1);
+//   $(this).text("Leave");
+//  });/// this and above might lag when called fast enough that is why below is perfect soln with alternat1 also
+
+
+
+  $(".blue-box").mouseenter(function () {
+    $(this).stop().fadeTo(2000, 0.5);
+    $(this).text("Enter");
   });
-  // ADDING CLICK LISTENER
-  $(".red-box").click(function (e) { 
-    e.preventDefault();
-    $(this).fadeTo(1000,0.5);
-  });
-  // triggering event listener
-  $(".red-box").click();
+  $(".blue-box").mouseleave(function () {
+    $(this).stop().fadeTo(2000, 1);
+    $(this).text("Leave");// either this or alternate 1 with stop()
+
+});
 
 });
