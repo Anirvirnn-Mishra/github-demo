9f948376beb17cb5071ec8adddf504441fb40a5a
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-22 01:56:17 +0530

    image gal lightbox

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-  // PASSING ADDITIONAL DATA TO EVENTS
+  var gal= $(".gallery").find("img");
+  gal.css("width","33%").css("opacity","0.7");
+  gal.mouseenter(function () { 
+    $(this).stop().css("opacity","1");
+  });
+  gal.mouseleave(function () { 
+    $(this).stop().css("opacity","0.7");
+  });
+  gal.click(
+    function()
+    {
+      
+      var source= $(this).attr("src");
+      var img= $("<img>").attr("src",source).css("width","100%");
+      $(".lightbox").empty().append(img).fadeIn(1000);
 
-  $("#btn-click").click(
-  {
-    user:"meow",
-    domain:"meowed.com",
-    "something":"someanswer"
-
-  },someFunc);// no neeed to pass any data
-function someFunc(params) { console.log(params.data.user) ; } 
+    });
+    $(".lightbox").click(
+      function()
+      {
+        
+        $(".lightbox").stop().fadeOut();
+  
+      });
 
 });
