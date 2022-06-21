b6b7063f7ed250d6f6e878dc01c8378148e9db77
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-22 02:55:07 +0530

    key down key up

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(function () {
        console.log(param.key);// LOGS KEY LETTER
        console.log(param.keyCode);// LOGS ASCII
        //if(param.key==="ArrowRight"){$(".blue-box").stop().animate({"margin-left":"+=5px"})}// lags too much 
-       if(param.key==="ArrowRight"){$(".blue-box").stop().css("margin-left","+=5px")} 
+       if(param.key==="ArrowRight"){$(".blue-box").stop().css("margin-left","+=5px")}// 
        if(param.key==="ArrowLeft"){$(".blue-box").css("margin-left","-=5px")}
 
     });
