7e486044e8607a9b1f498545dd80ab01e57c2491
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-06-21 12:41:54 +0530

    image swap with declared functions

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
-// on("click",function(){...})
-// $("html").on("click keydown", function (e/**optional event arg */) {
-//   //console.log($(e.offsetX));
-//   console.log(e.key );
-// });// calls everytime click or key is pressed
 
+  function ForImageSwap() {
+  
+
+    var images=["images/laptop-mobile_small.jpg","images/people-office-group-team_small.jpg","images/laptop-on-table_small.jpg"];
+      i=(i+1)%images.length;
+      $(this).fadeOut(function () {
+      $(this).attr("src",images[i]).fadeIn();}); 
+    
+      
+    }
+ 
 var i =0;
-var images=["images/laptop-mobile_small.jpg","images/people-office-group-team_small.jpg","images/laptop-on-table_small.jpg"];
-// $(".gallery").on("click", function () {
-//   i=(i+1)%images.length;
-//   $(this).find("img").first().attr("src",images[i]).fadeIn(200000); 
-//   //$(this).find("img").first().stop().attr("src",images[i]); // either can be usefd
 // });
-$(".gallery").find("img").first().on("click", function () {
-  i=(i+1)%images.length;
-  $(this).fadeOut(function () {
-  $(this).attr("src",images[i]).fadeIn();}); 
-  //$(this).find("img").first().stop().attr("src",images[i]); // either can be usefd
-});
+$(".gallery").find("img").first().on("click", ForImageSwap);
 
 
 
