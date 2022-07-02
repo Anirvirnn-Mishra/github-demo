aa8cac2fd8c6598b9cb50dda3d9efaed935fb810
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-07-02 15:46:07 +0530

    change

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $(function () {
 
-  // focus and blur
-  var inputs=$("input:text, input:password, textarea, input[type=email], input[type=checkbox]");
-  inputs.focus(function () { $(this).css("box-shadow","0 0 4px #666877"); });
-  inputs.focus(function (e) {
-    $(this).css("box-shadow","0 0 4px #666"); 
-  });
+ // change event
+ $("#check").change(function () { 
+  var ichecked=$(this).is(":checked");
+  if(ichecked){
+    $(this).add("label[for='check']").css("box-shadow","0 0 4px #181")
+  }
+  else
+  $(this).add("label[for='check']").css("box-shadow","0 0 4px #811")
 
-  inputs.blur(function () {console.log($(this).length<3);
-    if($(this).val.length<3)
-    {
-      $(this).css("box-shadow","0 0 4px #ff0000");
-
-    }
-    else
-     {$(this).css("box-shadow","none");
-     $(this).css("box-shadow","0 0 4px #00ff00");
-  }});
+  })
 });
